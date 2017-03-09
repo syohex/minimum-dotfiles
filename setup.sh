66ee66db2c34a1655f85f1f3447ffdf916e9c45e
Syohei YOSHIDA <syohex@gmail.com>
2017-03-09 12:26:58 +0900

    Use install command for making directory

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p $HOME/.zsh
+install -d $HOME/.zsh
 (cd $HOME/.zsh \
   && git clone https://github.com/zsh-users/zaw \
   && git clone https://github.com/zsh-users/zsh-completions)
